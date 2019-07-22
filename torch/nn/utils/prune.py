8da476e0bf9006866896a536439e41e6ae8ee4f2
Michela Paganini <micky.91@hotmail.com>
2019-07-21 21:42:53 -0700

    Add global unstructured pruning

--- torch/nn/utils/prune.py
+++ torch/nn/utils/prune.py
@@ -186,7 +186,7 @@ class PruningContainer(BasePruningMethod):
         elif len(args) == 1:  # only 1 item in a tuple
             setattr(self, '_tensor_name', args[0]._tensor_name)
             self.add_pruning_method(args[0])
-        else:  # manual construction from list or other iterable
+        else:  # manual construction from list or other iterable (or no args)
             for method in args:
                 self.add_pruning_method(method)
 
@@ -295,6 +295,10 @@ class PruningContainer(BasePruningMethod):
                 # create slice to identify what to prune
                 slc = [slice(None)] * n_dims
                 slc[axis] = keep_channel
+
+            elif method.PRUNING_TYPE == "global":
+                n_dims = len(t.shape)  # "is this a 2D tensor? 3D? ..."
+                slc = [slice(None)] * n_dims
 
             else:
                 raise ValueError(
@@ -689,6 +693,32 @@ class LnStructuredPruningMethod(BasePruningMethod):
             module, name, amount=amount, n=n, axis=axis)
 
 
+class CustomFromMaskPruningMethod(BasePruningMethod):
+
+    PRUNING_TYPE = "global"
+
+    def __init__(self, mask):
+        self.mask = mask
+
+    def compute_mask(self, t, default_mask):
+        assert default_mask.shape == self.mask.shape
+        mask = default_mask * self.mask.to(dtype=default_mask.dtype)
+        return mask
+
+    @classmethod
+    def apply(cls, module, name, mask):
+        """Adds the forward pre-hook that enables pruning on the fly and
+        the reparametrization of a tensor in terms of the original tensor
+        and the pruning mask.
+        Args:
+            module (nn.Module): module containing the tensor to prune
+            name (string): parameter name within `module` on which pruning
+                will act.
+        """
+        # this is here just for docstring generation for docs
+        return super(CustomFromMaskPruningMethod, cls).apply(module, name, mask)
+
+
 def identity(module, name):
     """Applies pruning reparametrization to the tensor corresponding to the
     parameter called `name` in `module` without actually pruning any units.
@@ -823,6 +853,103 @@ def ln_structured(module, name, amount, n, axis):
     LnStructuredPruningMethod.apply(module, name, amount, n, axis)
     return module
 
+
+def global_unstructured(parameters, pruning_method, **kwargs):
+    """
+    Args:
+        parameters (Iterable of (module, name) tuples): parameters of
+            the model to prune in a global fashion, i.e. by aggregating all
+            weights prior to deciding which ones to prune. module must be of
+            type nn.Module, and name must be a string.
+        pruning_method (function): a valid pruning function from this module, 
+            or a custom one implemented by the user that satisfies the 
+            implementation guidelines and has PRUNING_TYPE='unstructured'.
+        kwargs: other keyword arguments such as:
+            amount (int or float): quantity of parameters to prune across the 
+                specified parameters.
+                If float, should be between 0.0 and 1.0 and represent the
+                fraction of parameters to prune. If int, it represents the 
+                absolute number of parameters to prune.
+
+    Raises:
+        TypeError: if PRUNING_TYPE != 'unstructured'
+
+    Note:
+        Since global structured pruning doesn't make much sense unless the 
+        norm is normalized by the size of the parameter, we now limit the 
+        scope of global pruning to unstructured methods.
+    """
+    # ensure parameters is a list or generator of tuples
+    assert isinstance(parameters, Iterable)
+
+    # flatten parameter values to consider them all at once in global pruning
+    t = torch.nn.utils.parameters_to_vector([getattr(*p) for p in parameters])
+    # similarly, flatten the masks (if they exist), or use a flattened vector
+    # of 1s of the same dimensions as t
+    default_mask = torch.nn.utils.parameters_to_vector([
+        getattr(module, name + '_mask', torch.ones_like(getattr(module, name)))
+            for (module, name) in parameters
+    ])
+
+    # use the canonical pruning methods to compute the new mask, even if the
+    # parameter is now a flattened out version of `parameters`
+    container = PruningContainer()
+    container._tensor_name = 'temp'  # to make it match that of `method`
+    method = pruning_method(**kwargs)
+    method._tensor_name = 'temp' # to make it match that of `container`
+    if method.PRUNING_TYPE != 'unstructured':
+        raise TypeError('Only "unstructured" PRUNING_TYPE supported for '
+            'the `pruning_method`. Found method {} of type {}'.format(
+                pruning_method, method.PRUNING_TYPE))
+
+    container.add_pruning_method(method)
+
+    # use the `compute_mask` method from `PruningContainer` to combine the 
+    # mask computed by the new method with the pre-existing mask
+    final_mask = container.compute_mask(t, default_mask)
+
+    # Pointer for slicing the mask to match the shape of each parameter
+    pointer = 0
+    for module, name in parameters:
+
+        param = getattr(module, name)
+        # The length of the parameter
+        num_param = param.numel()
+        # Slice the mask, reshape it
+        param_mask = final_mask[pointer:pointer + num_param].view_as(param)
+        # Assign the correct pre-computed mask to each parameter and add it 
+        # to the forward_pre_hooks like any other pruning method
+        custom_from_mask(module, name, param_mask)
+
+        # Increment the pointer to continue slicing the final_mask
+        pointer += num_param
+        
+
+def custom_from_mask(module, name, mask):
+    """Prunes tensor corresponding to parameter called `name` in `module`
+    by applying the pre-computed mask in `mask`.
+    Modifies module in place (and also return the modified module) 
+    by:
+    1) adding a named buffer called `name+'_mask'` corresponding to the 
+    binary mask applied to the parameter `name` by the pruning method.
+    The parameter `name` is replaced by its pruned version, while the 
+    original (unpruned) parameter is stored in a new parameter named 
+    `name+'_orig'`.
+
+    Args:
+        module (nn.Module): module containing the tensor to prune
+        name (string): parameter name within `module` on which pruning
+                will act.
+        mask (Tensor): binary mask to be applied to the parameter.
+
+    Returns:
+        module (nn.Module): modified (i.e. pruned) version of the input
+            module, 
+    """
+    CustomFromMaskPruningMethod.apply(module, name, mask)
+    return module
+
+
 def remove(module, name):
     r"""Removes the pruning reparameterization from a module and the
     pruning method from the forward hook. The pruned
@@ -861,7 +988,7 @@ def is_pruned(model):
     """
     for _, module in model.named_module():
         for _, hook in module._forward_pre_hooks.items():
-            if isinstance(hook, prune.BasePruningMethod):
+            if isinstance(hook, BasePruningMethod):
                 return True
     return False
 
